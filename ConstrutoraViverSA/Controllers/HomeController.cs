394373e87c948e26aee7b053e32c871bac75b1be
mathe <matheus.eu.mv@gmail.com>
2021-06-01 21:56:07 -0300

    Criação projeto

---
+++ ConstrutoraViverSA/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using ConstrutoraViverSA.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace ConstrutoraViverSA.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Estoque()
+        {
+            return View();
+        }
+        public IActionResult Funcionarios()
+        {
+            return View();
+        }
+        public IActionResult Obras()
+        {
+            return View();
+        }
+        public IActionResult Orcamentos()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
