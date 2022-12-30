245fdfce62fc96a111f423d73f9189dacdedda2d
Matheus Vieira Santos <matheus.eu.mv@gmail.com>
2022-12-30 19:01:44 -0300

    Update 20221230204418_InitMigration.cs

--- src/ConstrutoraViverSA.Infrastructure/Migrations/20221230204418_InitMigration.cs
+++ src/ConstrutoraViverSA.Infrastructure/Migrations/20221230204418_InitMigration.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace ConstrutoraViverSA.Infrastructure.Migrations
 {
+    [ExcludeFromCodeCoverage]
     public partial class InitMigration : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
